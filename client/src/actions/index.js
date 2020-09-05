9f86d361ff2fcd4171f2c4b29ce2e3875941a316
Viktor Moroz <moroz@newtonideas.com>
2020-09-05 16:19:48 +0300

    stream edit submit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -33,11 +33,12 @@ export const fetchStream = (id) => async dispatch => {
 }
 
 export const deleteStream = (id) => async dispatch => {
-   await streams.delete(`/streams/${id}`);
-   dispatch({type: DELETE_STREAM, payload: id});
+    await streams.delete(`/streams/${id}`);
+    dispatch({type: DELETE_STREAM, payload: id});
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
-  dispatch({type: EDIT_STREAM, payload: response.data});
+    const response = await streams.patch(`/streams/${id}`, formValues);
+    dispatch({type: EDIT_STREAM, payload: response.data});
+    history.push('/');
 }
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {fetchStream} from "../../actions";
+import {fetchStream, editStream} from "../../actions";
+import StreamForm from "./StreamForm";
+import _ from 'lodash';
 
 class StreamEdit extends React.Component {
 
@@ -8,14 +10,18 @@ class StreamEdit extends React.Component {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues)
+    };
+
     render() {
-        console.log(this.props)
         if (!this.props.stream) {
             return <div>Loading...</div>;
         }
         return (
             <div>
-                {this.props.stream.title}
+                <h4>Edit {this.props.stream.title}</h4>
+                <StreamForm onSubmit={this.onSubmit} initialValues={_.pick(this.props.stream, 'title', 'description')} />
             </div>
         )
     }
@@ -25,4 +31,4 @@ const mapStateToProps = (state, ownProps) => {
     return {stream: state.streams[ownProps.match.params.id]};
 };
 
-export default connect(mapStateToProps, {fetchStream})(StreamEdit);
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
